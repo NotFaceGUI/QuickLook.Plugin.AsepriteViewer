f8d856761ee7b5bab089b17597a9d78289baf279
notface <61302498+NotFaceGUI@users.noreply.github.com>
2022-10-16 18:24:05 +0800

    Update Program.cs

--- AsepriteReadTest/Program.cs
+++ AsepriteReadTest/Program.cs
@@ -40,34 +40,5 @@ namespace AsepriteReadTest
             var value = ((string)software?.GetValue(""))?.Replace("\"", "").Replace("%1", "").TrimEnd();
             return value;
         }
-
-        public static Uri FilePathToFileUrl(string filePath)
-        {
-            var uri = new StringBuilder();
-            foreach (var v in filePath)
-                if (v >= 'a' && v <= 'z' || v >= 'A' && v <= 'Z' || v >= '0' && v <= '9' ||
-                    v == '+' || v == '/' || v == ':' || v == '.' || v == '-' || v == '_' || v == '~' ||
-                    v > '\x80')
-                    uri.Append(v);
-                else if (v == Path.DirectorySeparatorChar || v == Path.AltDirectorySeparatorChar)
-                    uri.Append('/');
-                else
-                    uri.Append($"%{(int)v:X2}");
-            if (uri.Length >= 2 && uri[0] == '/' && uri[1] == '/') // UNC path
-                uri.Insert(0, "file:");
-            else
-                uri.Insert(0, "file:///");
-
-            try
-            {
-                return new Uri(uri.ToString());
-            }
-            catch
-            {
-                return new Uri(filePath);
-            }
-        }
-    }
-
 }
 
